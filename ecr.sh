e3e400161f936b21d89cd4fccb0480b3c4a72e68
syeyoung <cyoung06@naver.com>
2024-07-22 14:24:17 +0900

    it finally works

--- ecr.sh
+++ ecr.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin aws_account_id.dkr.ecr.region.amazonaws.com
-docker build . -t public.ecr.aws/a7j5z8g6/embeddinggenerator:latest
-docker push public.ecr.aws/a7j5z8g6/embeddinggenerator:latest
+aws ecr get-login-password --region ap-northeast-2 --profile semicolon-inventory-test | docker login --username AWS --password-stdin 851725607847.dkr.ecr.ap-northeast-2.amazonaws.com
+docker build . -t 851725607847.dkr.ecr.ap-northeast-2.amazonaws.com/embeddinggen:v6
+docker push 851725607847.dkr.ecr.ap-northeast-2.amazonaws.com/embeddinggen:v6
--- main.py
+++ main.py
@@ -3,31 +3,36 @@ import os
 
 import base64
 # import requests
+import json
 
 from sentence_transformers import SentenceTransformer, util
 from PIL import Image
 
-print("Loadin clip model")
-model = SentenceTransformer('clip-ViT-B-32', cache_folder="/var/task/.cache")
-
-print("Loaded clip")
 
 import boto3
 
 s3 = boto3.client('s3')
 
+model = None
 def handler(event, context):
+    global model
+    if model is None:
+        print("Loadin clip model")
+        model = SentenceTransformer('clip-ViT-B-32', cache_folder="/var/task/.cache")
+
+        print("Loaded clip")
+    
     path = event["path"]
     image = None
     body = event["body"]
     if event["isBase64Encoded"]:
         body = base64.b64decode(body)
 
-    if path == '/processs3':
-        imageId = event["body"]["imageid"]
+    if path == 'processs3':
+        imageId = json.loads(body)["imageid"]
 
         bucket = os.getenv("BUCKET")
-        result = s3.get_object(Bucket = bucket, Prefix= imageId)
+        result = s3.get_object(Bucket = bucket, Key = imageId)
         contents = result['Body'].read()
 
         image = Image.open(io.BytesIO(contents))
@@ -39,4 +44,10 @@ def handler(event, context):
     img_emb = model.encode(image)
     payload = {"embedding": img_emb.tolist()}
 
-    return payload
+    return {
+        "statusCode": 200,
+        "headers": {
+            "Content-Type": "application/json"
+        },
+        "body": json.dumps(payload)
+    }
