ecd1e203b3ea5a65dd02c550a9441cad7c18fbe0
syeyoung <cyoung06@naver.com>
2024-08-05 11:43:03 +0900

    updat ecr path

--- ecr.sh
+++ ecr.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-aws ecr get-login-password --region ap-northeast-2 --profile semicolon-inventory-test | docker login --username AWS --password-stdin 851725607847.dkr.ecr.ap-northeast-2.amazonaws.com
-docker build . -t 851725607847.dkr.ecr.ap-northeast-2.amazonaws.com/embeddinggen:v6
-docker push 851725607847.dkr.ecr.ap-northeast-2.amazonaws.com/embeddinggen:v6
+aws ecr get-login-password --region ap-northeast-2 --profile semicolon-inventory | docker login --username AWS --password-stdin 767397837757.dkr.ecr.ap-northeast-2.amazonaws.com
+docker build . -t 767397837757.dkr.ecr.ap-northeast-2.amazonaws.com/embeddinggen:v1
+docker push 767397837757.dkr.ecr.ap-northeast-2.amazonaws.com/embeddinggen:v1
