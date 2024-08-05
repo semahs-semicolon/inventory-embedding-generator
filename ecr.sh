de5ac09555074caa66a0f573960af31c70e92037
syeyoung <cyoung06@naver.com>
2024-08-05 11:45:47 +0900

    also auto to public ecr for python image

--- ecr.sh
+++ ecr.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+aws ecr-public get-login-password --region us-east-1 --profile semicolon-inventory | docker login --username AWS --password-stdin public.ecr.aws
 aws ecr get-login-password --region ap-northeast-2 --profile semicolon-inventory | docker login --username AWS --password-stdin 767397837757.dkr.ecr.ap-northeast-2.amazonaws.com
 docker build . -t 767397837757.dkr.ecr.ap-northeast-2.amazonaws.com/embeddinggen:v1
 docker push 767397837757.dkr.ecr.ap-northeast-2.amazonaws.com/embeddinggen:v1
